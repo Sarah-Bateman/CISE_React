ba38859382ff13f63bf29561ecac707cfbeace3c
Sarah Bateman <sarahb3865@gmail.com>
2023-07-30 15:35:07 +1200

    Added github\directory and ci.yml file

--- Week_2_Worksheet/cise_ass1a_worksheet2-frontend/src/App.test.tsx
+++ Week_2_Worksheet/cise_ass1a_worksheet2-frontend/src/App.test.tsx
@@ -8,7 +8,7 @@ it("renders without crashing", () => {
 });
 
 describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
+  it('knows that 2 and 2 make 5', () => {
   expect(2 + 2).toBe(4);
   });
   });
--- Week_2_Worksheet/cise_ass1a_worksheet2-frontend/src/App.tsx
+++ Week_2_Worksheet/cise_ass1a_worksheet2-frontend/src/App.tsx
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
+
 interface State {
-count: number;
+  count: number;
 }
+
 class App extends Component<{}, State> {
-constructor(props: {}) {
-super(props);
-this.state = {
-count: 0,
-};
-}
-makeIncrementer = (amount: number) => () =>
-this.setState((prevState: State) => ({
-count: prevState.count + amount,
-}));
-increment = this.makeIncrementer(1);
-render() {
-return (
-<div>
-<p>Count: {this.state.count}</p>
-<button className="increment" onClick={this.increment}>
-Increment count
-</button>
-</div>
-);
-}
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  makeIncrementer = (amount: number) => () =>
+    this.setState((prevState: State) => ({
+      count: prevState.count + amount,
+    }));
+  increment = this.makeIncrementer(1);
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    );
+  }
 }
 export default App;
